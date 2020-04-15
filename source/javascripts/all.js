780395bf43fefca3c1b24497095a4c97ef4f805d
Jeremias Volker <jeremias.volker@googlemail.com>
2020-04-15 16:11:07 +0200

    Fixes scroll position when entering the site with anchor in url

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,19 +3,27 @@
 
 // smooth scroll to anchors with sticky header-notice
 
+function scrollToAnchor(_hash){
+  var $target = $(_hash);
+
+  var offset = $('#header-notice').outerHeight()
+
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top - offset
+  }, 100, 'swing', function () {
+    // window.location.hash = target;
+  });
+}
+
 $(document).ready(function () {
-     $(document).on('click','a[href^="#"]', function (e) {
-         e.preventDefault();
-
-         var offset = $('#header-notice').outerHeight()
-                  
-         var target = this.hash,
-             $target = $(target);
-
-         $('html, body').stop().animate({
-             'scrollTop': $target.offset().top - offset
-         }, 100, 'swing', function () {
-             // window.location.hash = target;
-         });
-     });
- });
+  if (window.location.hash) scrollToAnchor(window.location.hash);
+  
+  $(document).on('click','a[href^="#"]', function (e) {
+    e.preventDefault();
+            
+    scrollToAnchor(this.hash);
+  });
+});
+ 
+ 
+ // http://localhost:4567/?Raspberry%20Pi%20terminal#getting-started
